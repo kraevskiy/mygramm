46698c85e372d5be0d568da48a4858d3574f2aef
rambutan <pride.kraievskyi@gmail.com>
2023-12-11 12:09:41 +0200

    add Saved

--- src/_root/pages/Liked.tsx
+++ src/_root/pages/Liked.tsx
@@ -1,8 +1,23 @@
+import { GridPostList, Loader } from '@/components/shared';
+import { useGetCurrentUserQuery } from '@/lib/react-query/queries.ts';
 
 const Liked = () => {
+	const { data: currentUser } = useGetCurrentUserQuery();
 
+	if (!currentUser)
+		return (
+			<div className="flex-center w-full h-full">
+				<Loader />
+			</div>
+		);
 	return (
-		<div>Liked</div>
+		<>
+			{currentUser.liked.length === 0 && (
+				<p className="text-light-4">No liked posts</p>
+			)}
+
+			<GridPostList posts={currentUser.liked} showStats={false} />
+		</>
 	);
 };
 
